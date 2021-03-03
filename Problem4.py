2ed57881022a3cd42a8bde61000eeb02be653ba2
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2021-03-02 20:44:09 -0600

    update

--- Problem4.py
+++ Problem4.py
@@ -7,21 +7,21 @@ print(f"Length of the string {strInput} is {length}")
 if(length % 2 == 0):
     index = 0
     while(index < length):
-        print(strInput[index], end=' ')
+        print(strInput[index], end='\t')
         index += 2
     print()
     index = 0
     while(index < length):
-        print(index, end=' ')
+        print(index, end='\t')
         index += 2
 else:
     index = 1
     while(index < length):
-        print(strInput[index], end=' ')
+        print(strInput[index], end='\t')
         index += 2
     print()
     index = 1
     while(index < length):
-        print(index, end=' ')
+        print(index, end='\t')
         index += 2
 
