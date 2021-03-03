de8fb9cc6822a4e5db6aa29f02146923f3346fda
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2021-03-02 20:38:46 -0600

    last question

---
+++ Problem4.py
@@ -0,0 +1,27 @@
+strInput = input("gimme that input: ")
+
+length = len(strInput)
+
+print(f"Length of the string {strInput} is {length}")
+
+if(length % 2 == 0):
+    index = 0
+    while(index < length):
+        print(strInput[index], end=' ')
+        index += 2
+    print()
+    index = 0
+    while(index < length):
+        print(index, end=' ')
+        index += 2
+else:
+    index = 1
+    while(index < length):
+        print(strInput[index], end=' ')
+        index += 2
+    print()
+    index = 1
+    while(index < length):
+        print(index, end=' ')
+        index += 2
+
